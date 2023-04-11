9fadc447ff3a8ca804def4d170e46870936d6dfe
Libby Lewis <elewis43@jh.edu>
2023-04-10 23:35:43 -0500

    Centered initial map

--- app/src/main/java/lewis/libby/hw5/MainActivity.kt
+++ app/src/main/java/lewis/libby/hw5/MainActivity.kt
@@ -6,9 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 import lewis.libby.hw5.ui.theme.HW5Theme
+
+private val startHere = LatLng(38.9073, -77.0365)
+
+private val defaultCameraPosition = CameraPosition.fromLatLngZoom(startHere, 11f)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +35,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GoogleMap(
+                    val cameraPositionState = rememberCameraPositionState {
+                        position = defaultCameraPosition
+                    }
+
+                    GoogleMapDisplay(
+                        cameraPositionState = cameraPositionState,
                         modifier = Modifier.fillMaxSize(),
-                    ) {
-                        // no content yet
-                    }                   }
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun GoogleMapDisplay(
+    cameraPositionState: CameraPositionState,
+    modifier: Modifier
+) {
+
+//    LaunchedEffect(true) {
+//        val start =
+//            LatLng(38.9073, -77.0365)
+//        cameraPositionState.animate(
+//            CameraUpdateFactory.newLatLngZoom(
+//                start,
+//                16f
+//            ), 1000
+//        )
+//    }
+
+    GoogleMap(
+        cameraPositionState = cameraPositionState,
+        modifier = modifier,
+    ) {
+        // No content yet
     }
 }
